2e67f51352fe93f97019e9b9ad0fda4b90d71771
Jean <jdeyehe@gmail.com>
2019-05-30 13:12:50 +0200

    Security disable

--- src/main/java/com/esgi/heretoclean/security/SecurityConfig.java
+++ src/main/java/com/esgi/heretoclean/security/SecurityConfig.java
@@ -14,8 +14,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.authorizeRequests().anyRequest().authenticated();
+//		http.csrf().disable();
+//		http.authorizeRequests().anyRequest().authenticated();
 		
 	}
 	
