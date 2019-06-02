29d10dedb9b0ec7ac00c0563c5c303feba61855a
tdubreucq <thibaud.dubreucq@gmail.com>
2019-06-02 23:59:20 +0200

    Migration commit

--- src/main/java/com/esgi/heretoclean/web/AssociationController.java
+++ src/main/java/com/esgi/heretoclean/web/AssociationController.java
@@ -11,19 +11,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import com.esgi.heretoclean.models.Association;
 import com.esgi.heretoclean.service.interfaces.AssociationService;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/association")
 public class AssociationController {
