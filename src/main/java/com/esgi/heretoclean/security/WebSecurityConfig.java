fa192443882dfab73002cc29193133ab6975470e
DEYEHE <jdeyehe@gmail.com>
2019-07-06 17:24:04 +0200

    fix error 403

--- src/main/java/com/esgi/heretoclean/security/WebSecurityConfig.java
+++ src/main/java/com/esgi/heretoclean/security/WebSecurityConfig.java
@@ -4,8 +4,6 @@ package com.esgi.heretoclean.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -13,7 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+import com.esgi.heretoclean.security.NewFilter;
 
 
 
@@ -63,6 +61,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers("/api/**").authenticated()
 		.and() 
 		.apply(securityConfigurerAdapter());
+
 	}
 
 	private FirebaseJTWConfig securityConfigurerAdapter() {
