cf37972cbdfb9fc1819d8ead8af2f031920cd38f
dijouxd1993 <dijouxd1993@gmail.com>
2019-07-10 20:44:55 +0200

    -dur + fichier clé publique

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "aws-secgrp-allow-ssh" {
   name = "allow-ssh"
   
   ingress{
-      from_port =22
+      from_port =22 
       to_port = 22
       protocol ="tcp"
       cidr_blocks =["0.0.0.0/0"]
