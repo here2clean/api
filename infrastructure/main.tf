0d61905844705ceaf4756fd30dfefa522f36c057
dijouxd1993 <dijouxd1993@gmail.com>
2019-07-14 15:40:52 +0200

    correction elb

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -78,10 +78,11 @@ resource "aws_elb" "aws-loadbalancer" {
 
       health_check {
         healthy_threshold   = 2
-        unhealthy_threshold = 2
-        timeout             = 3
+        unhealthy_threshold = 10
+        timeout             = 30
         target              = "TCP:8085"
         interval            = 30
+        
     }
 
     instances = ["${aws_instance.aws-webserv.*.id}"]
